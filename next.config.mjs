4c2d96f8f71481a58b57bc36acf648243f9e2921
Chang Jia Ren <joy50422@gmail.com>
2024-08-09 10:09:39 +0800

    feat: configure basePath for static path

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === "production"
+
 const nextConfig = {
     output: 'export',
+    basePath: isProd ? "/next.js_static_export" : ""
 };
 
 export default nextConfig;
